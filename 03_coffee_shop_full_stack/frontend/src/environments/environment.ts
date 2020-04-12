1bf1b8898b04d86cee6cb3c9da3d5987ad203e75
MattiooFR <13595614+MattiooFR@users.noreply.github.com>
2020-04-12 19:25:49 +0200

    added the authentication config in the front end

--- 03_coffee_shop_full_stack/frontend/src/environments/environment.ts
+++ 03_coffee_shop_full_stack/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "mattioo.eu.auth0.com", // the auth0 domain prefix
+    url: "mattioo.eu", // the auth0 domain prefix
     audience: "coffeeshop", // the audience set for the auth0 app
     clientId: "o1kzeGLW94BQEsW951GZ7LEY4O2dB3T5", // the client id generated for the auth0 app
     callbackURL: "http://localhost:8100", // the base url of the running ionic application.
